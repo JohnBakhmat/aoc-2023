e84fcc6e45ff6072278a8e1bc4a4e24fa5159cdf
John Bakhmat <johnbakhmat@gmail.com>
2023-12-11 21:07:52 +0300

    i found the regex

--- day01/b.ts
+++ day01/b.ts
@@ -14,15 +14,14 @@ const words = [
 const reverse = (x: string) => x.split("").reverse().join("");
 
 const regex = new RegExp(`\\d|${words.join("|")}`);
-const reverseRegex = new RegExp(
-  `\\d|${(words.map(reverse)).join("|")}`,
-);
+const lastregex = new RegExp(`.*(\\d|${words.join("|")})`)
+
 const res = (await Deno.readTextFile("./input.txt"))
   .trim()
   .split("\n")
   .map((line: string) => {
     const a = line.match(regex)?.at(0)!;
-    const b = reverse(reverse(line).match(reverseRegex)?.at(0)!);
+    const b = line.match(lastregex)?.at(1);
     return [a, b].map((n) => n.length > 1 ? `${words.indexOf(n)}` : n).join("");
   })
   .reduce((a: number, c: string) => a + parseInt(c), 0);
